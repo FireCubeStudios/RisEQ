a3a3e5c71d69d1bf93ddd14cfdcc6fc0528ae609
FireCubeStudios <40955345+FireCubeStudios@users.noreply.github.com>
2022-02-20 00:16:51 +0100

    Add project files.

---
+++ RisEQ/Classes/SliderClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RisEQ.Classes
+{
+    public class SliderClass
+    {
+        public double Value { get; set; }
+    }
+}
---
+++ RisEQ/MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using RisEQ.Classes;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel.Core;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace RisEQ
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<SliderClass> Sliders = new ObservableCollection<SliderClass>();
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Sliders.Add(new SliderClass { Value = 12 });
+            Sliders.Add(new SliderClass { Value = 12 });
+            Sliders.Add(new SliderClass { Value = 12 });
+            Sliders.Add(new SliderClass { Value = 12 });
+            Sliders.Add(new SliderClass { Value = 12 });
+            Sliders.Add(new SliderClass { Value = 12 });
+            Sliders.Add(new SliderClass { Value = 12 });
+            SlidersList.ItemsSource = Sliders;
+            var titleBar = ApplicationView.GetForCurrentView().TitleBar;
+
+            titleBar.ButtonBackgroundColor = Colors.Transparent;
+            titleBar.ButtonInactiveBackgroundColor = Colors.Transparent;
+
+            // Hide default title bar.
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+
+            // Set XAML element as a draggable region.
+            Window.Current.SetTitleBar(AppTitleBar);
+        }
+
+    }
+}
